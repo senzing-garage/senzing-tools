62c5f325f232fb56f4c7152e7ceca72f45454276
docktermj <michael@dockter.com>
2023-03-13 15:07:50 -0400

    #5 Add config file support

--- cmd/root.go
+++ cmd/root.go
@@ -3,9 +3,11 @@
 package cmd
 
 import (
+	"fmt"
 	"os"
 
 	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
 )
 
 var (
@@ -14,12 +16,27 @@ var (
 	buildIteration    string = "0"
 )
 
+func makeVersion(version string, iteration string) string {
+	result := ""
+	if buildIteration == "0" {
+		result = version
+	} else {
+		result = fmt.Sprintf("%s-%s", buildVersion, buildIteration)
+	}
+	return result
+}
+
 var RootCmd = &cobra.Command{
 	Use:   "senzing-tools",
 	Short: "Tools to help use the Senzing API",
 	Long: `
 Welcome to senzing-tools!
 	`,
+	PreRun: func(cobraCommand *cobra.Command, args []string) {
+		versionTemplate := `{{printf "%s: %s - version %s\n" .Name .Short .Version}}`
+		cobraCommand.SetVersionTemplate(versionTemplate)
+	},
+	Version: makeVersion(buildVersion, buildIteration),
 }
 
 // Execute adds all child commands to the root command and sets flags appropriately.
@@ -28,5 +45,41 @@ func Execute() {
 	err := RootCmd.Execute()
 	if err != nil {
 		os.Exit(1)
+	}
+}
+
+func init() {
+	cobra.OnInitialize(initConfig)
+}
+
+// initConfig reads in config file and ENV variables if set.
+func initConfig() {
+	if configurationFile != "" {
+		// Use config file from the flag.
+		viper.SetConfigFile(configurationFile)
+	} else {
+
+		// Find home directory.
+
+		home, err := os.UserHomeDir()
+		cobra.CheckErr(err)
+
+		// Search config in home directory with name ".senzing-tools" (without extension).
+
+		viper.AddConfigPath(home + "/.senzing-tools")
+		viper.AddConfigPath(home)
+		viper.AddConfigPath("/etc/senzing-tools")
+		viper.SetConfigType("yaml")
+		viper.SetConfigName("senzing-tools")
+	}
+
+	// Read in environment variables that match "SENZING_TOOLS_*" pattern.
+
+	viper.AutomaticEnv()
+
+	// If a config file is found, read it in.
+
+	if err := viper.ReadInConfig(); err == nil {
+		fmt.Fprintln(os.Stderr, "Using config file:", viper.ConfigFileUsed())
 	}
 }
