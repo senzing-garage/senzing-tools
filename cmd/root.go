52270c5809bbc7b2ff28a90ada6f6270a26d45af
docktermj <michael@dockter.com>
2023-03-14 11:27:59 -0400

    #15 Savepoint

--- cmd/root.go
+++ cmd/root.go
@@ -6,6 +6,9 @@ import (
 	"fmt"
 	"os"
 
+	"github.com/senzing/senzing-tools/constant"
+	"github.com/senzing/senzing-tools/envar"
+	"github.com/senzing/senzing-tools/helper"
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
 )
@@ -19,16 +22,6 @@ var (
 	buildIteration string = "0"
 )
 
-func makeVersion(version string, iteration string) string {
-	result := ""
-	if buildIteration == "0" {
-		result = version
-	} else {
-		result = fmt.Sprintf("%s-%s", buildVersion, buildIteration)
-	}
-	return result
-}
-
 var RootCmd = &cobra.Command{
 	Use:   "senzing-tools",
 	Short: "Tools to help use the Senzing API",
@@ -37,10 +30,9 @@ Welcome to senzing-tools!
 	`,
 	PreRun: func(cobraCommand *cobra.Command, args []string) {
 		fmt.Println(">>>>> senzing-tools.PreRun")
-		versionTemplate := `{{printf "%s: %s - version %s\n" .Name .Short .Version}}`
-		cobraCommand.SetVersionTemplate(versionTemplate)
+		cobraCommand.SetVersionTemplate(constant.VersionTemplate)
 	},
-	Version: makeVersion(buildVersion, buildIteration),
+	Version: helper.MakeVersion(buildVersion, buildIteration),
 }
 
 // Execute adds all child commands to the root command and sets flags appropriately.
@@ -55,31 +47,30 @@ func Execute() {
 func init() {
 	fmt.Println(">>>>> senzing-tools.init()")
 	cobra.OnInitialize(initConfig)
-	RootCmd.PersistentFlags().String("configuration", defaultConfiguration, "Path to configuration file [SENZING_TOOLS_CONFIGURATION]")
+	RootCmd.PersistentFlags().String(constant.Configuration, defaultConfiguration, fmt.Sprintf("Path to configuration file [%s]", envar.Configuration))
 }
 
 // initConfig reads in config file and ENV variables if set.
 func initConfig() {
 	fmt.Println(">>>>> senzing-tools.initConfig()")
 	viper.AutomaticEnv()
-	viper.SetDefault("configuration", defaultConfiguration)
-	viper.BindPFlag("configuration", RootCmd.Flags().Lookup("configuration"))
 
-	if viper.GetString("configuration") != "" {
-		viper.SetConfigFile(viper.GetString("configuration"))
-	} else {
+	configuration := RootCmd.Flags().Lookup(constant.Configuration).Value.String()
+	if configuration != "" { // Use configuration file specified as a command line option.
+		viper.SetConfigFile(configuration)
+	} else { // Search for a configuration file.
 
-		// Find home directory.
+		// Determine home directory.
 
 		home, err := os.UserHomeDir()
 		cobra.CheckErr(err)
 
-		// Configuration file: senzing-tools.yaml
+		// Specify configuration file name.
 
 		viper.SetConfigName("senzing-tools")
 		viper.SetConfigType("yaml")
 
-		// Search path order.
+		// Define search path order.
 
 		viper.AddConfigPath(home + "/.senzing-tools")
 		viper.AddConfigPath(home)
