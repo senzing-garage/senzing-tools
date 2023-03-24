146ad49e7a430ebb6a73219394d5e9bd8f95cd6b
roncewind <dad@lynntribe.net>
2023-03-24 11:10:02 -0700

    #22 update envar constants

--- envar/main.go
+++ envar/main.go
@@ -8,6 +8,7 @@ const (
 	Configuration           = "SENZING_TOOLS_CONFIGURATION"
 	DatabaseUrl             = "SENZING_TOOLS_DATABASE_URL"
 	Datasources             = "SENZING_TOOLS_DATASOURCES"
+	DelayInSeconds          = "SENZING_TOOLS_DELAY_IN_SECONDS"
 	EnableG2config          = "SENZING_TOOLS_ENABLE_G2CONFIG"
 	EnableG2configmgr       = "SENZING_TOOLS_ENABLE_G2CONFIGMGR"
 	EnableG2diagnostic      = "SENZING_TOOLS_ENABLE_G2DIAGNOSTIC"
@@ -17,5 +18,8 @@ const (
 	EngineLogLevel          = "SENZING_TOOLS_ENGINE_LOG_LEVEL"
 	EngineModuleName        = "SENZING_TOOLS_ENGINE_MODULE_NAME"
 	GrpcPort                = "SENZING_TOOLS_GRPC_PORT"
+	InputFileType           = "SENZING_TOOLS_INPUT_FILE_TYPE"
+	InputURL                = "SENZING_TOOLS_INPUT_URL"
 	LogLevel                = "SENZING_TOOLS_LOG_LEVEL"
+	OutputURL               = "SENZING_TOOLS_OUTPUT_URL"
 )
