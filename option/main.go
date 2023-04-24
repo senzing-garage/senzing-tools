4af96a60fda8fa56b682c670174a0241a1ebba37
Ron Lynn <dad@lynntribe.net>
2023-04-24 10:45:26 -0700

    Issue 22.ron.0 (#39)
    * save point
    
    * save point
    
    * save point
    
    * #22 update envar constants

--- option/main.go
+++ option/main.go
@@ -8,6 +8,7 @@ const (
 	Configuration           = "configuration"
 	DatabaseUrl             = "database-url"
 	Datasources             = "datasources"
+	DelayInSeconds          = "delay-in-seconds"
 	EnableG2config          = "enable-g2config"
 	EnableG2configmgr       = "enable-g2configmgr"
 	EnableG2diagnostic      = "enable-g2diagnostic"
@@ -17,5 +18,20 @@ const (
 	EngineLogLevel          = "engine-log-level"
 	EngineModuleName        = "engine-module-name"
 	GrpcPort                = "grpc-port"
+	InputFileType           = "input-file-type"
+	InputURL                = "input-url"
 	LogLevel                = "log-level"
+	OutputURL               = "output-url"
+)
+
+// ----------------------------------------------------------------------------
+// Constant help text for parameters
+// ----------------------------------------------------------------------------
+
+const (
+	DelayInSecondsHelp = "Number of seconds to wait before starting process"
+	InputFileTypeHelp  = "Input file type to override auto-detect based on file name"
+	InputURLHelp       = "Input URL used for processing"
+	LogLevelHelp       = "Log level of TRACE, DEBUG, INFO, WARN, ERROR, FATAL, or PANIC [%s]"
+	OutputURLHelp      = "Output URL used for processing"
 )
