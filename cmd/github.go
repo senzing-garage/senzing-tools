ce20aa764474268332b0747077012ac1bfe3943f
docktermj <docktermj@users.noreply.github.com>
2023-06-19 15:19:33 +0000

    make-go-github-file.yaml updated cmd/github.go for versioned release: 0.3.1

--- cmd/github.go
+++ cmd/github.go
@@ -1,11 +1,12 @@
-// 0.2.9
-// Created by make-go-github-file.yaml on Fri May 26 21:09:51 UTC 2023
+// 0.3.1
+// Created by make-go-github-file.yaml on Mon Jun 19 15:19:33 UTC 2023
 package cmd
 
-var githubDate string = "2023-06-19"
-var githubIteration string = "0"
-var githubRef string = "refs/tags/0.3.0"
-var githubRefName string = "0.3.0"
-var githubRepository string = "Senzing/senzing-tools"
-var githubRepositoryName string = "senzing-tools"
-var githubVersion string = "0.3.0"
+var githubDate            string = "2023-06-19"
+var githubIteration       string = "0"
+var githubRef             string = "refs/tags/0.3.1"
+var githubRefName         string = "0.3.1"
+var githubRepository      string = "Senzing/senzing-tools"
+var githubRepositoryName  string = "senzing-tools"
+var githubVersion         string = "0.3.1"
+
