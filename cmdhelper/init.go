05542191ea1c36cc008c6f5b072b5322f965ba2a
roncewind <dad@lynntribe.net>
2023-07-19 12:03:06 -0700

    #75 added SetCobraFlag

--- cmdhelper/init.go
+++ cmdhelper/init.go
@@ -11,16 +11,19 @@ import (
 func Init(cobraCommand *cobra.Command, contextVariables []option.ContextVariable) {
 
 	for _, contextVariable := range contextVariables {
-		switch contextVariable.Type {
-		case optiontype.Bool:
-			cobraCommand.Flags().Bool(contextVariable.Arg, contextVariable.Default.(bool), fmt.Sprintf(contextVariable.Help, contextVariable.Envar))
-		case optiontype.Int:
-			cobraCommand.Flags().Int(contextVariable.Arg, contextVariable.Default.(int), fmt.Sprintf(contextVariable.Help, contextVariable.Envar))
-		case optiontype.String:
-			cobraCommand.Flags().String(contextVariable.Arg, contextVariable.Default.(string), fmt.Sprintf(contextVariable.Help, contextVariable.Envar))
-		case optiontype.StringSlice:
-			cobraCommand.Flags().StringSlice(contextVariable.Arg, contextVariable.Default.([]string), fmt.Sprintf(contextVariable.Help, contextVariable.Envar))
-		}
+		SetCobraFlag(cobraCommand, contextVariable)
+	}
+}
 
+func SetCobraFlag(cobraCommand *cobra.Command, contextVariable option.ContextVariable) {
+	switch contextVariable.Type {
+	case optiontype.Bool:
+		cobraCommand.Flags().Bool(contextVariable.Arg, contextVariable.Default.(bool), fmt.Sprintf(contextVariable.Help, contextVariable.Envar))
+	case optiontype.Int:
+		cobraCommand.Flags().Int(contextVariable.Arg, contextVariable.Default.(int), fmt.Sprintf(contextVariable.Help, contextVariable.Envar))
+	case optiontype.String:
+		cobraCommand.Flags().String(contextVariable.Arg, contextVariable.Default.(string), fmt.Sprintf(contextVariable.Help, contextVariable.Envar))
+	case optiontype.StringSlice:
+		cobraCommand.Flags().StringSlice(contextVariable.Arg, contextVariable.Default.([]string), fmt.Sprintf(contextVariable.Help, contextVariable.Envar))
 	}
 }
