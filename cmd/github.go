584c6f23a5b35801281e1e58ed043500d97895f5
roncewind <roncewind@users.noreply.github.com>
2023-07-28 13:01:19 +0000

    make-go-github-file.yaml updated cmd/github.go for versioned release: 0.4.1

--- cmd/github.go
+++ cmd/github.go
@@ -1,11 +1,13 @@
-// 0.3.1
-// Created by make-go-github-file.yaml on Mon Jun 19 15:19:33 UTC 2023
+// 0.4.1
+// Created by make-go-github-file.yaml on Fri Jul 28 13:01:19 UTC 2023
 package cmd
 
-var githubDate string = "2023-06-19"
-var githubIteration string = "0"
-var githubRef string = "refs/tags/0.3.1"
-var githubRefName string = "0.3.1"
-var githubRepository string = "Senzing/senzing-tools"
-var githubRepositoryName string = "senzing-tools"
-var githubVersion string = "0.3.1"
+var (
+	githubDate           string = "2023-07-28"
+	githubIteration      string = "0"
+	githubRef            string = "refs/tags/0.4.1"
+	githubRefName        string = "0.4.1"
+	githubRepository     string = "Senzing/senzing-tools"
+	githubRepositoryName string = "senzing-tools"
+	githubVersion        string = "0.4.1"
+)
