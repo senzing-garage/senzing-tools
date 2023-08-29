cd9b314a6b90c8af62e9bfb1470924935288eacd
docktermj@users.noreply.github.com <docktermj@users.noreply.github.com@users.noreply.github.com>
2023-08-29 21:30:57 +0000

    make-go-github-file.yaml updated cmd/github.go for versioned release: 0.4.4

--- cmd/github.go
+++ cmd/github.go
@@ -1,11 +1,15 @@
-// 0.3.1
-// Created by make-go-github-file.yaml on Mon Jun 19 15:19:33 UTC 2023
+// 0.4.4
+// Created by make-go-github-file.yaml on Tue Aug 29 21:30:57 UTC 2023
+//
+//lint:file-ignore U1000 Ignore all unused code, it's generated
 package cmd
 
-var githubDate string = "2023-06-19"
-var githubIteration string = "0"
-var githubRef string = "refs/tags/0.3.1"
-var githubRefName string = "0.3.1"
-var githubRepository string = "Senzing/senzing-tools"
-var githubRepositoryName string = "senzing-tools"
-var githubVersion string = "0.3.1"
+var (
+	githubDate           string = "2023-08-29"
+	githubIteration      string = "0"
+	githubRef            string = "refs/tags/0.4.4"
+	githubRefName        string = "0.4.4"
+	githubRepository     string = "Senzing/senzing-tools"
+	githubRepositoryName string = "senzing-tools"
+	githubVersion        string = "0.4.4"
+)
