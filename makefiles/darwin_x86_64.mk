66a8483e03c09c1e9c32eeebb01a0d87a694e336
docktermj <michael@dockter.com>
2024-01-04 17:05:27 -0500

    #164 Support building on arm64

---
+++ makefiles/darwin_x86_64.mk
@@ -0,0 +1,16 @@
+# Makefile extensions for darwin-x86_64.
+
+# -----------------------------------------------------------------------------
+# OS-ARCH specific targets
+# -----------------------------------------------------------------------------
+
+.PHONY: build-osarch-specific
+build-osarch-specific: darwin/amd64
+
+# -----------------------------------------------------------------------------
+# Makefile targets supported only by this platform.
+# -----------------------------------------------------------------------------
+
+.PHONY: only-darwin-x86_64
+only-darwin-x86_64:
+	@echo "Only darwin-x86_64 has this Makefile target."
